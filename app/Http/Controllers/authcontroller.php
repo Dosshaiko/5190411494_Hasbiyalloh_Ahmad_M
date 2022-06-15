e4a77a1815d979a55f1b0052b8e9f2607ccec1bb
Dosshaiko <astronaut190101@gmail.com>
2022-06-15 17:30:52 +0700

    first commit

---
+++ app/Http/Controllers/authcontroller.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+        return view('register');
+    }
+
+    public function simpanuser(Request $request)
+    {
+        $user = User::create([
+            'nama' => $request->nama,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return redirect('/register');
+    }
+
+    public function login()
+    {
+        return view('login');
+    }
+}
